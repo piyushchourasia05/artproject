21a36632ce2a9815f6b15c81ab0cd93bea2aee71
piyushchourasia05 <78714786+piyushchourasia05@users.noreply.github.com>
2023-04-24 13:45:13 +0530

    Initial library import

---
+++ ml.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import random
+sns.set()
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras import layers
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+
+# libraries for recognising images
+import skimage.io
+import skimage.transform
+from PIL import Image
+from scipy import ndimage
+
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from keras.preprocessing.image import ImageDataGenerator
+
+import os
+
+# Data Collection phase
+# Data exploration
+#  data visualisation
+
+#Model Building
+
+
+# resize image
+
+
+
