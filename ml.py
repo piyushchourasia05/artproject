fb5d9006ee8733988937814d790761d8828c6988
piyushchourasia05 <78714786+piyushchourasia05@users.noreply.github.com>
2023-05-15 20:26:32 +0530

    Update ml.py

--- ml.py
+++ ml.py
@@ -1,36 +1,43 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import random
-sns.set()
-from tensorflow import keras
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
-from tensorflow.keras import layers
-from keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
-
-# libraries for recognising images
-import skimage.io
-import skimage.transform
-from PIL import Image
-from scipy import ndimage
-
-import tensorflow as tf
-from tensorflow.python.framework import ops
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
 
-import os
+# Set the path to your dataset
+train_data_path = '/path/to/train_data'
+test_data_path = '/path/to/test_data'
 
-# Data Collection phase
-# Data exploration
-#  data visualisation
+# Define the dimensions of the input images and the number of classes
+input_shape = (64, 64, 3)
+num_classes = 2
 
-#Model Building
+# Data augmentation for training set
+train_data_generator = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 
+# Data augmentation for test set (only rescaling)
+test_data_generator = ImageDataGenerator(rescale=1./255)
 
-# resize image
+# Load and preprocess training data
+train_data = train_data_generator.flow_from_directory(train_data_path, target_size=input_shape[:2], batch_size=32, class_mode='categorical')
 
+# Load and preprocess test data
+test_data = test_data_generator.flow_from_directory(test_data_path, target_size=input_shape[:2], batch_size=32, class_mode='categorical')
 
+# Define the CNN architecture
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
 
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
+model.fit(train_data, epochs=10, validation_data=test_data)
+
+# Save the trained model
+model.save('pattern_recognition_model.h5')
